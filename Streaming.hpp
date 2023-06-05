82184c862dc5a2544544d7085610abcfddcb506e
Gabrieltadeu1327 <129296951+Gabrieltadeu1327@users.noreply.github.com>
2023-06-05 14:35:22 -0300

    Update Streaming.hpp

--- Streaming.hpp
+++ Streaming.hpp
@@ -8,8 +8,7 @@ class Streaming {
 public:
     std::vector<Video*> catalogo;
 
-    void cadastrar_filme(Video* v);
-    void cadastrar_serie(Video* v);
+    void cadastrar_video(Video* v);
     void avaliacao(int id, int nota);
     void print_catalogo();
 
